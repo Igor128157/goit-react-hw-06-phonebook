e5a83d7fb4565a273db3e60b62bf294a093816dc
Igor Verovka <igor0063@ukr.net>
2020-07-05 16:20:56 +0300

    Updates

---
+++ precache-manifest.dd79c35a7433fa1b50e70e17a0cd0fb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c0a823134a9bb280d3b3c1f02419bf",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "fa6d1375cbbb90a84977",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.322f335b.chunk.css"
+  },
+  {
+    "revision": "5a343e114f8141219496",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.a18721a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.a18721a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa6d1375cbbb90a84977",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.dcfb4d37.chunk.js"
+  },
+  {
+    "revision": "9d1a26fb56dbe9d98a08",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b0a8c527.js"
+  }
+]);
